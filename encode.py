ef8b3fd3c759df15fb838e885f207101b939caf7
Michael Seymour <michael.seymour208@gmail.com>
2026-09-07 15:27:17 -0600

    message bit stream

--- encode.py
+++ encode.py
@@ -4,3 +4,6 @@ import scipy.io as sio
 # Reading in .wav file & secret message
 testFile = sio.wavfile.read('test.wav')
 secret = input("Message to encode: ")
+
+# Converting the message string into a stream of bits
+msgBits = ''.join(format(ord(char), '08b') for char in secret)
