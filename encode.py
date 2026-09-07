9482eb1876feb057da9df9e4b9962bbbc30d6658
Michael Seymour <michael.seymour208@gmail.com>
2026-09-07 16:36:30 -0600

    Writing the encoded data to a new wav file

--- encode.py
+++ encode.py
@@ -28,5 +28,8 @@ for channel in data:
     for sample in channel:
         # (sample & ~1) - Clears LSB of sample
         # (sample & ~1) | secret bit - Populates LSB with our data
-        (sample & ~1) | secret[i]
+        (sample & ~1) | int(secret[i])
     i += 1
+
+
+sio.wavfile.write('encodedFile.wav', rate, data)
