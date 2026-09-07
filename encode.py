291250044f9439ec4d1d58cf7ffe9093dc936b4c
Michael Seymour <michael.seymour208@gmail.com>
2026-09-07 15:40:47 -0600

    Added a fixed length header and combined msgBits and header

--- encode.py
+++ encode.py
@@ -3,7 +3,14 @@ import scipy.io as sio
 
 # Reading in .wav file & secret message
 testFile = sio.wavfile.read('test.wav')
-secret = input("Message to encode: ")
+msg = input("Message to encode: ")
 
 # Converting the message string into a stream of bits
-msgBits = ''.join(format(ord(char), '08b') for char in secret)
+msgBits = ''.join(format(ord(char), '08b') for char in msg)
+
+# Adding a 32 bit header to the secret data to denote message size in bits
+# So the decoder knows when to stop reading from encoded file
+header = np.binary_repr(len(msg) * 8, width=32)
+secret = header + msgBits
+print(secret)
+
