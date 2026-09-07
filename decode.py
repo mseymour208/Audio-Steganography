1f3b9ccb480c8b79ae41bfa374b84e64f5259545
Michael Seymour <michael.seymour208@gmail.com>
2026-09-07 17:00:06 -0600

    See if the encoded message will print in binary

--- decode.py
+++ decode.py
@@ -8,19 +8,23 @@ buffer, header = ""
 # Iterate data -> channel -> sample
 i = 0
 for channel in encodedData:
-    # We reach the end of the header
+    # We've reached the end of the header
     if (i == 31):
-        # Find length of encoded data
+        # Find length of encoded data & clear out buffer
         header = int(buffer, 2)
         buffer = ""
         i = 0
 
+    # We've reached the end of encoded data
     if (i == (header - 1)):
         break
+
     for sample in channel:
         # Extract LSB
         LSB = (sample & 1)
         buffer += LSB
+
+print(buffer)
 
 # iterate till i = 31, appending bits to buffer
 # Convert buffer to int to get header
