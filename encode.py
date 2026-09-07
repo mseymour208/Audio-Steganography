7cf10b22925469e99d57bd7e6aa3154c223669f2
Michael Seymour <michael.seymour208@gmail.com>
2026-09-07 15:20:32 -0600

    Making sure .wav is 16 bit PCM

--- encode.py
+++ encode.py
@@ -2,6 +2,5 @@ import numpy as np
 import scipy.io as sio
 
 # Reading in .wav file & secret message
-testFile = sio.wavefile.read('test.wav')
+testFile = sio.wavfile.read('test.wav')
 secret = input("Message to encode: ")
-print(secret)
