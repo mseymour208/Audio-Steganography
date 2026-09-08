399fb49868e3b7805b9baed6d5cfb40994c9d1aa
Michael Seymour <michael.seymour208@gmail.com>
2026-09-07 20:08:07 -0600

    Done for today

--- encode.py
+++ encode.py
@@ -1,10 +1,11 @@
 import numpy as np
 import scipy.io as sio
+import customtkinter as ctk
 
-# Reading in .wav file & secret message
+# Reading in .wav file
 rate, data = sio.wavfile.read('test.wav')
 
-# Reading message from file
+# Reading secret message from file
 fileName = input("Name of txt file: ") + '.txt'
 with open(fileName, 'r', encoding='utf-8') as file:
     msg = file.read()
@@ -18,18 +19,12 @@ msgBits = ''.join(format(b, '08b') for b in msgBytes)
 header = np.binary_repr(len(msgBytes) * 8, width=32)
 secret = header + msgBits
 i = len(secret)
-#print(secret)
 
-# I want to encode both channels so I can maximize capacity
-# I want to iterate first through each sample, then through to both channels
-# Creating a 1D view of data
-# Extracting the first i elements
-# Applying (sample & ~1) to clear LSB of first i elements
-# .wav file is stereo, so two channels (N, 2)
-# Flattening so I can iterate linearlly
-# I clear each LSB and embed data, then overwrite the memory at flatView[idx]
+# Create a 1D view of data to iterate linearlly
 flatView = data.ravel()
 for idx in range(i):
+    # Embedding one bit at a time
+    # First clearing the LSB, then inserting our secret bit
     embedded = (flatView[idx] & ~np.int16(1)) | int(secret[idx])
     flatView[idx] = np.int16(embedded)
 
