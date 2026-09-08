00cf989184a8a57fa8f2f858140b7a4a8d754caa
Michael Seymour <michael.seymour208@gmail.com>
2026-09-07 18:27:24 -0600

    Looks about done, have to make documentation changes

--- decode.py
+++ decode.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.io as sio
+from bitarray import bitarray
 
 # Read in encoded wav
 rate, encodedData = sio.wavfile.read('encodedFile.wav')
@@ -21,7 +22,14 @@ for idx in range(32, header+32):
     bit = (flatView[idx] & np.int16(1))
     buffer += str(bit)
 
-print(buffer)
+# Convert the buffer into ASCII
+# Feed binary message into a bitarray
+ba = bitarray(buffer)
+
+# Turn into bytes and decode into ASCII
+decodedMsg = ba.tobytes().decode('ascii')
+print(decodedMsg)
+
 
 # iterate till i = 31, appending bits to buffer
 # Convert buffer to int to get header
